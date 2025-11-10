dc1073f37098a41e42fc8041555d4f6aba4d74a7
ddurumboy2197 <ddurumboy2197@gmail.com>
2025-11-10 16:59:07 +0500

    added file

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title = "Home page")
+
+
+@app.route('/error')
+def error404():
+    return render_template('404.html', title = "Error 404")
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = "About")
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', title = "Blog")
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title = "Contact")
+
+
+@app.route('/price')
+def price():
+    return render_template('price.html', title = "Price")
+
+
+@app.route('/project')
+def project():
+    return render_template('project.html', title = "Project")
+
+
+@app.route('/service')
+def service():
+    return render_template('service.html', title = "Service")
+
+
+@app.route('/team')
+def team():
+    return render_template('team.html', title = "Team")
+
+
+@app.route('/testimonial')
+def testimonial():
+    return render_template('testimonial.html', title = "Testimonial")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
